03a71864eab5502fdca244ad8c125a6a7d5e4b70
Georg Daniel Vahtramäe <48998862+GeorgDV@users.noreply.github.com>
2020-02-21 10:37:12 +0200

    2 commit

--- bin/run.js
+++ bin/run.js
@@ -1,7 +1,9 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const download = require('image-downloader');
 
-axios.get('https://xkcd.com/')
+for(i=2270; i>=2260;i--){
+axios.get('https://xkcd.com/'+ i +'/')
 .then(function(response) {
     //console.log(response);
     const $ = cheerio.load(response.data)
@@ -9,4 +11,15 @@ axios.get('https://xkcd.com/')
     console.log(title);
     let imgURL=$('#comic img').attr('src');
     console.log(imgURL)
-})
+
+    const options = {
+        url: 'https:'+imgURL,
+        dest: 'img'                // Save to /path/to/dest/image.jpg
+      }
+       
+      download.image(options)
+        .then(({ filename, image }) => {
+          console.log('Saved to', filename)  // Saved to /path/to/dest/image.jpg
+        })
+        .catch((err) => console.error(err))
+})}
